999f29d06874fc1d4899e511e1f5ef591af6f0c7
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-06-15 17:56:12 +0300

    Функция для создания кубиков

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ $start.addEventListener('click', startGame)
 function startGame() {
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
+
+    renderBox()
+}
+
+function renderBox() {
+
 }
