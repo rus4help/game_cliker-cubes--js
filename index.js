5dd48bcbfa9a7b659353cad79611d6f3dca0205c
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-06-15 18:08:17 +0300

    Создал заготовку куба

--- index.js
+++ index.js
@@ -11,5 +11,15 @@ function startGame() {
 }
 
 function renderBox() {
+    var box = document.createElement('div')
+
+    box.style.height = box.style.width = '50px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = '50px'
+    box.style.left = '70px'
+    box.style.cursor = 'pointer'
+
+    $game.insertAdjacentElement('afterbegin', box)
 
 }
