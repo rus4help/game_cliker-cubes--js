d9f01585e9579ecd1300da2a430aa086f381a04f
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-06-15 18:38:24 +0300

    Доделал рандомную динамическую генерацию кубов

--- index.js
+++ index.js
@@ -24,12 +24,15 @@ function renderBox() {
     $game.innerHTML = ''
     var box = document.createElement('div')
     var boxSize = getRandom(30, 100)
+    var gameSize = $game.getBoundingClientRect()
+    var maxTop = gameSize.height - boxSize
+    var maxLeft = gameSize.width - boxSize
 
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
     box.style.backgroundColor = '#000'
-    box.style.top = '50px'
-    box.style.left = '70px'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
     box.setAttribute('data-box', 'true')
 
